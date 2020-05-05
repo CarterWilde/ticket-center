29f285e4c3fa3ea5ee9fd5e954d93f4040b00588
CarterWilde <carterjwilde@gmail.com>
2020-05-05 16:19:54 -0600

    Refactored Icon

--- src/Components/Icon.tsx
+++ src/Components/Icon.tsx
@@ -5,10 +5,14 @@ export default class Icon{
     this.text = text;
     this.classNames = classNames;
   }
-
+  /**
+   * CSS Friendly className list
+   * @param prefix string to be prefixed before classNames
+   * @returns a string of all the classNames concatinated together that's CSS friendly
+   */
   getClassNames(prefix?: string) : string {
     return prefix ? prefix : '' + (this.classNames ?
-    this.classNames.map(className => { return className })
+    this.classNames.map(className => { return ' ' + className })
     :null);
   }
 }
