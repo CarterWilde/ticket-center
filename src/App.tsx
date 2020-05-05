9a8cfbcb98ec1a0ce500946244661d8bcc9c88bd
CarterWilde <carterjwilde@gmail.com>
2020-05-04 21:20:39 -0600

    Added Fab to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+
 import './App.css';
+
+import Fab from './Components/Fab';
 
 function App() {
   return (
     <div className="App">
+      <Fab 
+        icon={{text: '+', classNames: []}}
+        content="New Ticket"/>
     </div>
   );
 }
